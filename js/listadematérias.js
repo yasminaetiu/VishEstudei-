bde0510186484e21f595e3d84c08e5cacff614b0
isabelly <isabelly@egmail.com>
2025-05-21 16:54:51 -0300

    arrumando o potugues

--- js/listadematérias.js
+++ js/listadematérias.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Redirecionamento para a página de Matemática
     document.getElementById('btnMath').addEventListener('click', function() {
         window.location.href = "./mat.html";
+    });
+
+    // Redirecionamento para a página de Português
+    document.getElementById('btnPort').addEventListener('click', function() {
+        window.location.href = "./port.html";
     });
 });
 
