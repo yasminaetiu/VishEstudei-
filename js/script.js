41318fcf9a66d29a719cc1d398565ee2df47c856
isabelly <isabelly@egmail.com>
2025-05-21 16:05:27 -0300

    projeto

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('btnMath').addEventListener('click', function() {
+        window.location.href = "./mat.html";
+    });
+});
+
---
+++ js/seleçãodeaulas.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('btnMath').addEventListener('click', function() {
+        window.location.href = "./mat.html";
+    });
+});
