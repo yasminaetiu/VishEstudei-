b56b40b60766577be53ad5fe654c6e5369d9e8f5
Your Name <you@example.com>
2025-05-22 17:16:57 -0300

    fisica

--- js/listadematérias.js
+++ js/listadematérias.js
@@ -18,5 +18,13 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('btnQuim').addEventListener('click', function() {
         window.location.href = "./quím.html";
     });
+
+    // Redirecionamento para Física
+    var btnFis = document.getElementById("btnFis");
+    if (btnFis) {
+        btnFis.addEventListener("click", function() {
+            window.location.href = "fís.html";
+        });
+    }
 });
 
