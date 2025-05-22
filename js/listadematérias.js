7091af6009e009192a5013e1173167c85e48ad1b
Your Name <isabelly@gmail.com>
2025-05-22 14:42:25 -0300

    arrumando o química

--- js/listadematérias.js
+++ js/listadematérias.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Redirecionamento para Matemática (exemplo)
+    // Redirecionamento para Matemática
     document.getElementById('btnMath').addEventListener('click', function() {
         window.location.href = "./mat.html";
     });
     
-    // Redirecionamento para Português (exemplo)
+    // Redirecionamento para Português
     document.getElementById('btnPort').addEventListener('click', function() {
         window.location.href = "./port.html";
     });
@@ -12,6 +12,11 @@ document.addEventListener("DOMContentLoaded", function() {
     // Redirecionamento para Biologia
     document.getElementById('btnBio').addEventListener('click', function() {
         window.location.href = "./bio.html";
+    });
+    
+    // Você também pode adicionar aqui o listener para Química
+    document.getElementById('btnQuim').addEventListener('click', function() {
+        window.location.href = "./quím.html";
     });
 });
 
